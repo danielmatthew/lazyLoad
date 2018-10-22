771c55028adb7fa9e9754debc1e86d42c497cd1a
Dan Matthew <danielmatthew@users.noreply.github.com>
2018-10-22 21:51:55 +0200

    Initial commit

---
+++ src/components/ImageItem.vue
@@ -0,0 +1,64 @@
+<template>
+  <figure class="image__wrapper">
+    <img 
+      class="image__item"
+      :data-url="source"
+      alt=""
+    >
+  </figure>
+</template>
+
+<script>
+export default {
+  name: "ImageItem",
+  props: {
+    source: {
+      type: String,
+      required: true
+    }
+  },
+  inserted: el => {
+    function loadImage() {
+      const imageElement = Array.from(el.children).find(
+        el => el.nodeName === 'IMG'
+      );
+
+      if (imageElement) {
+        imageElement.addEventListener('load', () => {
+          setTimeout(() => el.classList.add('loaded'), 100);
+        });
+
+        imageElement.addEventListener('error', () => console.log('error'));
+        
+        imageElement.src = imageElement.dataset.url;
+      }
+    }
+
+    function handleIntersect(entries, observer) {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          loadImage();
+          observer.unobserve(el);
+        }
+      })
+    }
+
+    function createObserver() {
+      const options = {
+        root: null,
+        threshold: '0'
+      };
+
+      const observer = new IntersectionObserver(handleIntersect, options);
+
+      observer.observe(el);
+    }
+
+    if (window["IntersectionObserver"]) {
+      createObserver();
+    } else {
+      loadImage();
+    }
+  }
+};
+</script>
